8c675f1cfc522ef81d543c24d6f44edb74dd8721
Shawn Koski <shawnnkoski@gmail.com>
2018-03-29 00:12:13 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-  "nice"
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
